0cd1d3c100d68e812edefc4321f457f58c4d7fd2
Pavel Dreit <pavel@dreit.ru>
2020-04-10 00:06:55 +0500

    Add header

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,1 +1,40 @@
+(()=>{
+
+	var mainHeader = document.querySelector('header.main-header');
+
+	window.addEventListener('scroll', function() {
+		// eslint-disable-next-line
+		window.throttling(function() {
+			var scrolled = window.pageYOffset || document.documentElement.scrollTop;
+			if (scrolled > 5) {
+				mainHeader.classList.add('unsticked');
+			} else {
+				mainHeader.classList.remove('unsticked');
+			}
+		})
+	}, false);
+
+	let menuBtn = mainHeader.querySelector('.menu-btn');
+
+	function toggleMenu(bool) {
+		window.lockScroll(bool);
+		menuBtn.classList.toggle('active', bool);
+		mainHeader.classList.toggle('opened', bool);
+		document.getElementsByTagName('body')[0].classList.toggle('show-menu', bool);
+	}
+
+	menuBtn.addEventListener('click', ()=>{
+
+		if (!mainHeader.classList.contains('opened')) {
+			toggleMenu(true);
+			window.setOutsideClickListener(mainHeader, ()=>{
+				toggleMenu(false);
+			});
+		} else {
+			toggleMenu(false);
+		}
+
+	}, false);
+
+})()
 
--- src/main.js
+++ src/main.js
@@ -9,6 +9,60 @@ import './components/header/header.js';
 /*=====  End of components  ======*/
 
 
+window.throttling = (()=>{
+	let forLastExec,
+		delay = 100, // delay between calls
+		throttled = false;
+
+	return function(cb) {
+		// only run if we're not throttled
+		if (!throttled) {
+			// actual callback action
+			cb();
+			// we're throttled!
+			throttled = true;
+			// set a timeout to un-throttle
+			setTimeout(function() {
+				throttled = false;
+			}, delay);
+		}
+		// last exec on resize end
+		clearTimeout(forLastExec);
+		forLastExec = setTimeout(cb, delay);
+	}
+})()
+
+window.lockScroll = (state =true)=>{
+	let body = document.getElementsByTagName('body')[0];
+	if (state) {
+		let scrollWidth = window.innerWidth - document.documentElement.clientWidth;
+		body.classList.add('scroll-locked');
+		body.style.overflowX = 'hidden';
+		body.style.overflowY = 'hidden';
+		body.style.paddingRight = scrollWidth + 'px';
+	} else {
+		body.classList.remove('scroll-locked');
+		body.style.overflowX = 'auto';
+		body.style.overflowY = 'scroll';
+		body.style.paddingRight = 0;
+	}
+}
+
+window.setOutsideClickListener = (target, cb)=>{
+	let body = document.getElementsByTagName('body')[0];
+	let theTarget = typeof target == 'string' ? body.querySelector(target) : target;
+	body.removeEventListener('mouseup', task, false);
+	let task = (e)=>{
+		if (!theTarget.contains(e.target)) {
+			cb();
+			e.target.removeEventListener('mouseup', task, false)
+		}
+	}
+	body.addEventListener('mouseup', task, false);
+}
+
+
+
 document.querySelectorAll('a[href="#"], a[href=""], a[href="https://example.com/"]').forEach((elem)=>{
 	elem.addEventListener('click', function (e) {
 		e.preventDefault();
