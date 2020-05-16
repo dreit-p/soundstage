85fae75d054465e277e52ce93732d15a00af8f9c
Pavel Dreit <pavel@dreit.ru>
2020-05-16 15:36:10 +0500

    Add menus

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -16,27 +16,67 @@
 		})
 	}, false);
 
-	let menuBtn = mainHeader.querySelector('.menu-btn');
+	mainHeader.querySelector('.veil').addEventListener('click', function () {
+		closeMenu();
+	}, false);
 
-	function toggleMenu(bool) {
+	function toggleWrapper(bool) {
 		window.lockScroll(bool);
-		menuBtn.classList.toggle('active', bool);
 		mainHeader.classList.toggle('opened', bool);
 		document.getElementsByTagName('body')[0].classList.toggle('show-menu', bool);
 	}
 
-	menuBtn.addEventListener('click', ()=>{
+	function setMenuContent(type) {
+		document.querySelectorAll('.menu-wrapper.active').forEach((menu)=>{
+			menu.classList.remove('active');
+		});
+		if (type) {
+			let wrapper = document.querySelector(`.menu-wrapper.${type}`);
+			wrapper.classList.add('active');
+			window.setOutsideClickListener(mainHeader, closeMenu);
+		}
+	}
 
-		if (!mainHeader.classList.contains('opened')) {
-			toggleMenu(true);
-			mainHeader.querySelector('.veil').addEventListener('click', function (e) {
-				toggleMenu(false);
-			}, false);
-		} else {
-			toggleMenu(false);
+	function closeMenu() {
+		toggleWrapper(false);
+		setMenuContent(false);
+		document.querySelectorAll('.active[data-select-menu]').forEach((link)=>{
+			link.classList.toggle('active', false)
+		});
+	}
+
+	function selectMenu(type) {
+
+		if (!type) {
+			closeMenu();
+			return false;
 		}
 
-	}, false);
+		((activeMenu)=>{
+			if (!!activeMenu && activeMenu.classList.contains(type)) {
+				closeMenu();
+				return false;
+			}
+			toggleWrapper(true);
+			setMenuContent(type);
+			document.querySelectorAll(`[data-select-menu="${type}"]`).forEach((link)=>link.classList.toggle('active', true));
+		})(document.querySelector('.menu-wrapper.active'))
+
+	}
+
+
+	document.querySelectorAll('[data-select-menu]').forEach((link)=>{
+		link.addEventListener('click', ()=>{
+			selectMenu(link.dataset.selectMenu);
+		}, false)
+	});
+
+	[].forEach.call(document.querySelectorAll("a.profile-menu"), (menuLink) => {
+		menuLink.addEventListener('click', function (e) {
+			e.preventDefault();
+
+		}, false);
+	});
 
 })()
 
