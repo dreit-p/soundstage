d275ae4bd3aa9eadda892f9d0398050d9809d861
Pavel Dreit <pavel@dreit.ru>
2020-07-20 23:48:36 +0500

    Make header build functions global

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -4,75 +4,91 @@
 
 	if (!mainHeader) return false;
 
-	function updateHeaderSticking() {
-		var scrolled = window.pageYOffset || document.documentElement.scrollTop;
-		mainHeader.classList.toggle('unsticked', scrolled > 5);
-	}
+	function initHeader () {
 
-	window.addEventListener('scroll', function() {
-		window.throttling(updateHeaderSticking)
-	}, false);
-
-	window.addEventListener('load', function () {
-		updateHeaderSticking();
-	}, false);
-
-	mainHeader.querySelector('.veil').addEventListener('click', function () {
-		closeMenu();
-	}, false);
-
-	function toggleWrapper(bool) {
-		window.lockScroll(bool);
-		mainHeader.classList.toggle('opened', bool);
-		document.getElementsByTagName('body')[0].classList.toggle('show-menu', bool);
-	}
-
-	function setMenuContent(type) {
-		document.querySelectorAll('.menu-wrapper.active').forEach((menu)=>{
-			menu.classList.remove('active');
-		});
-		if (type) {
-			let wrapper = document.querySelector(`.menu-wrapper.${type}`);
-			wrapper.classList.add('active');
-			window.setOutsideClickListener(mainHeader, closeMenu);
+		function updateHeaderSticking() {
+			var scrolled = window.pageYOffset || document.documentElement.scrollTop;
+			mainHeader.classList.toggle('unsticked', scrolled > 5);
 		}
-	}
 
-	function closeMenu() {
-		toggleWrapper(false);
-		setMenuContent(false);
-		document.querySelectorAll('.active[data-select-menu]').forEach((link)=>{
-			link.classList.toggle('active', false)
-		});
-	}
+		window.addEventListener('scroll', function() {
+			window.throttling(updateHeaderSticking)
+		}, false);
 
-	function selectMenu(type) {
+		window.addEventListener('load', function () {
+			updateHeaderSticking();
+		}, false);
 
-		if (!type) {
+		mainHeader.querySelector('.veil').addEventListener('click', function () {
 			closeMenu();
-			return false;
+		}, false);
+
+		function toggleWrapper(bool) {
+			window.lockScroll(bool);
+			mainHeader.classList.toggle('opened', bool);
+			document.getElementsByTagName('body')[0].classList.toggle('show-menu', bool);
 		}
 
-		((activeMenu)=>{
-			if (!!activeMenu && activeMenu.classList.contains(type)) {
+		function setMenuContent(type) {
+			document.querySelectorAll('.menu-wrapper.active').forEach((menu)=>{
+				menu.classList.remove('active');
+			});
+			if (type) {
+				let wrapper = document.querySelector(`.menu-wrapper.${type}`);
+				wrapper.classList.add('active');
+				window.setOutsideClickListener(mainHeader, closeMenu);
+			}
+		}
+
+		function closeMenu() {
+			toggleWrapper(false);
+			setMenuContent(false);
+			document.querySelectorAll('.active[data-select-menu]').forEach((link)=>{
+				link.classList.toggle('active', false)
+			});
+		}
+
+		function selectMenu(type) {
+
+			if (!type) {
 				closeMenu();
 				return false;
 			}
-			toggleWrapper(true);
-			setMenuContent(type);
-			document.querySelectorAll(`[data-select-menu="${type}"]`)
-				.forEach((link)=>link.classList.toggle('active', true));
-		})(document.querySelector('.menu-wrapper.active'))
 
+			((activeMenu)=>{
+				if (!!activeMenu && activeMenu.classList.contains(type)) {
+					closeMenu();
+					return false;
+				}
+				toggleWrapper(true);
+				setMenuContent(type);
+				document.querySelectorAll(`[data-select-menu="${type}"]`)
+					.forEach((link)=>link.classList.toggle('active', true));
+			})(document.querySelector('.menu-wrapper.active'))
+
+		}
+
+
+		document.querySelectorAll('[data-select-menu]').forEach((link)=>{
+			link.addEventListener('click', (e)=>{
+				e.preventDefault();
+				selectMenu(link.dataset.selectMenu);
+			}, false)
+		});
 	}
+	initHeader();
 
-
-	document.querySelectorAll('[data-select-menu]').forEach((link)=>{
-		link.addEventListener('click', (e)=>{
-			e.preventDefault();
-			selectMenu(link.dataset.selectMenu);
-		}, false)
-	});
+	window.rebuildHeader = ()=>{
+		let old_element = mainHeader;
+		let new_element = old_element.cloneNode(true);
+		old_element.parentNode.replaceChild(new_element, old_element);
+		mainHeader = new_element;
+		initHeader();
+	}
+	window.initHeader = (header = mainHeader)=>{
+		mainHeader = header;
+		initHeader();
+	}
 
 })()
 
